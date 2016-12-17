a19c7dfa34d495a7b354515e4ca20a7aa6b1fdb7
Arthur Cordova <arthur.stapassoli@gmail.com>
2016-12-17 01:26:23 -0200

    table view

---
+++ provaios/FilmeCell.swift
@@ -0,0 +1,15 @@
+//
+//  FilmeCell.swift
+//  provaios
+//
+//  Created by Arthur on 17/12/16.
+//  Copyright © 2016 Arthur. All rights reserved.
+//
+
+import UIKit
+
+class FilmeCell: UITableViewCell {
+
+    @IBOutlet var titulo: UILabel!
+   
+}
--- provaios/ViewController.swift
+++ provaios/ViewController.swift
@@ -11,21 +11,32 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate {
 
     @IBOutlet var buttonLogin: UIButton!
+    @IBOutlet var table: UITableView!
     
     
-    func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
-        cell.textLabel?.text = "Arthur"
-        return cell;
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath)
+        -> UITableViewCell {
+            
+            let cell = self.table.dequeueReusableCell(withIdentifier: "cell") as! FilmeCell
+            //let agendamento = self.agendamentos[indexPath.row]
+            
+            cell.titulo.text = "Arthur 01"
+           
+            
+            return cell
     }
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let xib = UINib(nibName: "FilmeCell", bundle: nil)
+        self.table.register(xib, forCellReuseIdentifier: "cell")
     }
     
     override func didReceiveMemoryWarning() {
