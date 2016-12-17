569a4aab6071112b6727384c780b7a83eef2f077
Arthur Cordova <arthur.stapassoli@gmail.com>
2016-12-17 00:47:16 -0200

    Commit inicial

---
+++ provaios/Canal.swift
@@ -0,0 +1,18 @@
+//
+//  Canal.swift
+//  provaios
+//
+//  Created by Arthur on 17/12/16.
+//  Copyright © 2016 Arthur. All rights reserved.
+//
+
+import Foundation
+
+class Canal {
+    
+    var titulo: String!
+    var subTitulo: String!
+    var duracao: String!
+    var sinopse: String!
+    
+}
--- provaios/ViewController.swift
+++ provaios/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttonLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
