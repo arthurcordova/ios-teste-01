1353fd717ffb291fd53b2b79b2e2295ed44ded3a
Arthur Cordova <arthur.stapassoli@gmail.com>
2016-12-18 14:35:51 -0200

    Filme table

--- provaios/FilmesViewController.swift
+++ provaios/FilmesViewController.swift
@@ -23,31 +23,23 @@ class FilmesViewController: UIViewController, UITableViewDataSource, UITableView
         let xib = UINib(nibName: "FilmeCellTableViewCell", bundle: nil)
         self.table.register(xib, forCellReuseIdentifier: cellIdentifier)
         
-        
         table.delegate = self
         table.dataSource = self
     }
     
-    // number of rows in table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.animals.count
     }
     
-    // create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        // create a new cell if needed or reuse an old one
         let cell:FilmeCellTableViewCell = table.dequeueReusableCell(withIdentifier: cellIdentifier) as! FilmeCellTableViewCell!
-        
-        // set the text from the data model
         cell.label1.text = self.animals[indexPath.row]
         
         return cell
     }
     
-    // method to run when table view cell is tapped
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("You tapped cell number \(indexPath.row).")
+        print("Linha \(indexPath.row).")
     }
 
 
